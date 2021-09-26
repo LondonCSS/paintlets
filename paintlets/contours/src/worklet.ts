092ea03b46f6f6d4ff60dd3feb639a8accaa9e57
Oliver Turner <oliver.turner@codedsignal.co.uk>
2021-09-26 02:17:57 +0100

    tweaks

--- paintlets/contours/src/worklet.ts
+++ paintlets/contours/src/worklet.ts
@@ -3,9 +3,6 @@ import * as houdini from "../../../typings/houdini";
 type Area = [number, number];
 type Point = [number, number];
 type PolyLine = Point[];
-type Props = typeof defaultProps;
-type PropKey = keyof Props;
-type Transform = (v?: string) => string | number;
 
 import SimplexNoise from "simplex-noise";
 import { mapRange, linspace } from "canvas-sketch-util/math";
@@ -26,9 +23,8 @@ function drawShape([start, ...pts]: PolyLine) {
   return [start, ...pts, start];
 }
 
-function drawIsoLines(lineSpace: number, gridUnit: number) {
-  return function (noiseData: PolyLine[], [sizeX, sizeY]: Area): PolyLine[] {
-    const intervals = linspace(lineSpace, gridUnit);
+function getIsoLineFn(intervals: number[], gridUnit: number, [sizeX, sizeY]: Area) {
+  return function (noiseData: PolyLine[]): PolyLine[] {
     const lines: PolyLine[] = [];
 
     intervals.forEach((_: any, idx: number) => {
@@ -39,12 +35,10 @@ function drawIsoLines(lineSpace: number, gridUnit: number) {
         isoBands(noiseData, lowerBand, upperBand - lowerBand, {
           successCallback(bands: PolyLine[]) {
             bands.forEach((band: PolyLine) => {
-              const scaledBand: PolyLine = band.map(([x, y]) => {
-                return [
-                  mapRange(x, 0, gridUnit - 1, 0, sizeX),
-                  mapRange(y, 0, gridUnit - 1, 0, sizeY),
-                ];
-              });
+              const scaledBand: PolyLine = band.map(([x, y]) => [
+                mapRange(x, 0, gridUnit - 1, 0, sizeX),
+                mapRange(y, 0, gridUnit - 1, 0, sizeY),
+              ]);
 
               lines.push(drawShape(scaledBand));
             });
@@ -59,6 +53,20 @@ function drawIsoLines(lineSpace: number, gridUnit: number) {
   };
 }
 
+function makeNoise(gridUnit: number): PolyLine[] {
+  const noiseData: PolyLine[] = [];
+  for (let y = 0; y < gridUnit; y++) {
+    noiseData[y] = [];
+    for (let x = 0; x < gridUnit; x++) {
+      const _n = simplex.noise2D(x / (gridUnit * 0.75), y / (gridUnit * 0.75));
+      const n = mapRange(_n, -1, 1, 0, 1);
+      noiseData[y].push(n);
+    }
+  }
+
+  return noiseData;
+}
+
 export class Contours implements houdini.PaintCtor {
   static get inputProperties() {
     return inputProps;
@@ -70,30 +78,16 @@ export class Contours implements houdini.PaintCtor {
     props: houdini.StylePropertyMapReadOnly
   ) {
     const { width, height } = size;
-
     const gridUnit = +(props.get("--grid-unit") || defaultProps.gridUnit);
     const lineWidth = +(props.get("--line-width") || defaultProps.lineWidth);
     const lineFrequency = +(props.get("--line-frequency") || defaultProps.lineFrequency);
-    const lineColour = props.get("--line-colour")?.length > 0
-      ? [...props.get("--line-colour")][0]
-      : defaultProps.lineColour;
+    const lineColour = String(props.get("--line-colour")).trim() || defaultProps.lineColour;
 
-    console.log(props.get("--line-colour"));
+    const intervals = linspace(lineFrequency, gridUnit);
+    const drawIsoLines = getIsoLineFn(intervals, gridUnit, [width, height]);
 
-
-    const noiseData: PolyLine[] = [];
-    for (let y = 0; y < gridUnit; y++) {
-      noiseData[y] = [];
-      for (let x = 0; x < gridUnit; x++) {
-        const _n = simplex.noise2D(x / (gridUnit * 0.75), y / (gridUnit * 0.75));
-
-        const n = mapRange(_n, -1, 1, 0, 1);
-
-        noiseData[y].push(n);
-      }
-    }
-
-    const lines = drawIsoLines(lineFrequency, gridUnit)(noiseData, [width, height]);
+    const noiseData = makeNoise(gridUnit);
+    const lines = drawIsoLines(noiseData);
 
     ctx.strokeStyle = lineColour;
     ctx.lineWidth = lineWidth;
@@ -101,9 +95,7 @@ export class Contours implements houdini.PaintCtor {
     lines.forEach(([start, ...pts]: PolyLine) => {
       ctx.beginPath();
       ctx.moveTo(...start);
-
       pts.forEach((pt) => ctx.lineTo(...pt));
-
       ctx.stroke();
     });
   }
