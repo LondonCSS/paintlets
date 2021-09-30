1837bc9cd9aa92bd5f05bda37b994a9c92104d49
Oliver Turner <oliver.turner@codedsignal.co.uk>
2021-09-30 20:27:28 +0100

    Relax restrictions on top-level await

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,24 @@
  * @typedef {(import("vite").UserConfigExport)} UserConfigExport
  */
 
+// import fs from "fs";
 import path from "path";
 import { URL } from "url";
 import { defineConfig } from "vite";
 
-const root = new URL(".", import.meta.url).pathname;
+const rootPath = new URL(".", import.meta.url).pathname;
+const examplePath = path.join(rootPath, "examples");
+// const pages = fs.readdirSync(examplePath)
 
 export default defineConfig({
   build: {
+    target: "esnext", // support top-level await
     rollupOptions: {
       input: {
-        main: path.resolve(root, "index.html"),
-        contour: path.resolve(root, "examples/contour.html"),
-        seigaiha: path.resolve(root, "examples/seigaiha.html"),
-        truchet: path.resolve(root, "examples/truchet.html"),
+        main: path.resolve(rootPath, "index.html"),
+        contour: path.resolve(rootPath, "examples/contour.html"),
+        seigaiha: path.resolve(rootPath, "examples/seigaiha.html"),
+        truchet: path.resolve(rootPath, "examples/truchet.html"),
       },
     },
   },
