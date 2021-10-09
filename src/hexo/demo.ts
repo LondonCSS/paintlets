805df69636a1ba6595477db6e88d779cac6db9a2
Oliver Turner <oliver.turner@codedsignal.co.uk>
2021-10-09 18:01:07 +0100

    Make colours more configurable

--- src/hexo/demo.ts
+++ src/hexo/demo.ts
@@ -2,19 +2,30 @@ import { init } from "../_lib/demo";
 
 init("hexo");
 
-const customProps = ["--style", "--radius", "--hue", "--gap", "--stroke-width", "--stroke-colour"];
+const sampleProps = [
+  "background-color",
+  "--style",
+  "--radius",
+  "--hue",
+  "--gap",
+  "--stroke-width",
+  "--stroke-colour",
+];
 
 document.addEventListener("click", (event) => {
   const el = event.target as HTMLElement;
   const li = el?.closest("li");
   if (li) {
     const props: Record<string, unknown> = {};
-    for (const prop of customProps) {
+    for (const prop of sampleProps) {
       const computedProp = getComputedStyle(li).getPropertyValue(prop);
       if (computedProp) {
-        props[prop] = computedProp;
+        props[prop] = computedProp.trim();
       }
     }
-    console.log(props);
+
+    props["background-image"] = "paint(hexo)";
+
+    Object.assign(document.body.style, props);
   }
 });
