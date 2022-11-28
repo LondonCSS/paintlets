2af6b1916c921c32e2291564b2cce4dbef92c363
Oliver Turner <oliverturner@users.noreply.github.com>
2022-11-28 18:57:46 +0000

    Update deps

--- src/paintlets/contour/src/worklet.ts
+++ src/paintlets/contour/src/worklet.ts
@@ -1,121 +1,123 @@
-import * as houdini from "../../../../typings/houdini";
-
-import { normaliseInput } from "../../../_lib/utils";
+import type * as houdini from "../../../../typings/houdini";
+import type { NoiseFunction2D } from "simplex-noise";
 
 type Area = [number, number];
 type Point = [number, number];
 type PolyLine = Point[];
 
-import SimplexNoise from "simplex-noise";
+import { createNoise2D } from "simplex-noise";
 import { mapRange, linspace } from "canvas-sketch-util/math";
 import { clipPolylinesToBox } from "canvas-sketch-util/geometry";
 import { isoBands } from "marchingsquares";
 
+import { normaliseInput } from "../../../_lib/utils";
+
 type PaintletProps = {
-  gridUnit: number;
-  lineColour: string;
-  lineWidth: number;
-  lineFrequency: number;
+	gridUnit: number;
+	lineColour: string;
+	lineWidth: number;
+	lineFrequency: number;
 };
 
 export const defaultProps = {
-  "--grid-unit": {
-    key: "gridUnit",
-    value: 192,
-    parseAs: "int",
-  },
-  "--line-colour": {
-    key: "lineColour",
-    value: "#fff",
-    parseAs: "string",
-  },
-  "--line-width": {
-    key: "lineWidth",
-    value: 0.1,
-    parseAs: "float",
-  },
-  "--line-frequency": {
-    key: "lineFrequency",
-    value: 12,
-    parseAs: "int",
-  },
+	"--grid-unit": {
+		key: "gridUnit",
+		value: 192,
+		parseAs: "int",
+	},
+	"--line-colour": {
+		key: "lineColour",
+		value: "#fff",
+		parseAs: "string",
+	},
+	"--line-width": {
+		key: "lineWidth",
+		value: 0.1,
+		parseAs: "float",
+	},
+	"--line-frequency": {
+		key: "lineFrequency",
+		value: 12,
+		parseAs: "int",
+	},
 } as const;
 
 function drawShape([start, ...pts]: PolyLine) {
-  return [start, ...pts, start];
+	return [start, ...pts, start];
 }
 
 function getIsoLineFn(intervals: number[], gridUnit: number, [sizeX, sizeY]: Area) {
-  return function (noiseData: PolyLine[]): PolyLine[] {
-    const lines: PolyLine[] = [];
+	return function (noiseData: PolyLine[]): PolyLine[] {
+		const lines: PolyLine[] = [];
 
-    intervals.forEach((_: number, idx: number) => {
-      if (idx > 0) {
-        const lowerBand = intervals[idx - 1];
-        const upperBand = intervals[idx];
+		intervals.forEach((_: number, idx: number) => {
+			if (idx > 0) {
+				const lowerBand = intervals[idx - 1];
+				const upperBand = intervals[idx];
 
-        isoBands(noiseData, lowerBand, upperBand - lowerBand, {
-          successCallback(bands: PolyLine[]) {
-            bands.forEach((band: PolyLine) => {
-              const scaledBand: PolyLine = band.map(([x, y]) => [
-                mapRange(x, 0, gridUnit - 1, 0, sizeX),
-                mapRange(y, 0, gridUnit - 1, 0, sizeY),
-              ]);
+				isoBands(noiseData, lowerBand, upperBand - lowerBand, {
+					successCallback(bands: PolyLine[]) {
+						bands.forEach((band: PolyLine) => {
+							const scaledBand: PolyLine = band.map(([x, y]) => [
+								mapRange(x, 0, gridUnit - 1, 0, sizeX),
+								mapRange(y, 0, gridUnit - 1, 0, sizeY),
+							]);
 
-              lines.push(drawShape(scaledBand));
-            });
-          },
-          noQuadTree: true,
-          noFrame: true,
-        });
-      }
-    });
+							lines.push(drawShape(scaledBand));
+						});
+					},
+					noQuadTree: true,
+					noFrame: true,
+				});
+			}
+		});
 
-    return clipPolylinesToBox(lines, [0, 0, sizeX, sizeY]);
-  };
+		return clipPolylinesToBox(lines, [0, 0, sizeX, sizeY]);
+	};
 }
 
-function makeNoise(simplex: SimplexNoise, gridUnit: number): PolyLine[] {
-  const noiseData: PolyLine[] = [];
-  for (let y = 0; y < gridUnit; y++) {
-    noiseData[y] = [];
-    for (let x = 0; x < gridUnit; x++) {
-      const _n = simplex.noise2D(x / (gridUnit * 0.75), y / (gridUnit * 0.75));
-      const n = mapRange(_n, -1, 1, 0, 1);
-      noiseData[y].push(n);
-    }
-  }
+function makeNoise(noise2D: NoiseFunction2D, gridUnit: number): PolyLine[] {
+	const noiseData: PolyLine[] = [];
+	for (let y = 0; y < gridUnit; y++) {
+		noiseData[y] = [];
+		for (let x = 0; x < gridUnit; x++) {
+			const _n = noise2D(x / (gridUnit * 0.75), y / (gridUnit * 0.75));
+			const n = mapRange(_n, -1, 1, 0, 1);
+			noiseData[y].push(n);
+		}
+	}
 
-  return noiseData;
+	return noiseData;
 }
 
 export class Contour implements houdini.PaintCtor {
-  public static defaultProperties = defaultProps;
-  public static inputProperties = Object.keys(Contour.defaultProperties);
+	public static defaultProperties = defaultProps;
+	public static inputProperties = Object.keys(Contour.defaultProperties);
 
-  paint(
-    ctx: houdini.PaintRenderingContext2D,
-    { width, height }: houdini.PaintSize,
-    rawProps: houdini.StylePropertyMapReadOnly
-  ): void {
-    const props = normaliseInput(rawProps, Contour) as PaintletProps;
-    const { gridUnit, lineWidth, lineFrequency, lineColour } = props;
-    const simplex = new SimplexNoise();
+	paint(
+		ctx: houdini.PaintRenderingContext2D,
+		{ width, height }: houdini.PaintSize,
+		rawProps: houdini.StylePropertyMapReadOnly
+	): void {
+		const props = normaliseInput(rawProps, Contour) as PaintletProps;
+		const { gridUnit, lineWidth, lineFrequency, lineColour } = props;
 
-    const intervals = linspace(lineFrequency, gridUnit);
-    const drawIsoLines = getIsoLineFn(intervals, gridUnit, [width, height]);
+		const noise2D = createNoise2D();
 
-    const noiseData = makeNoise(simplex, gridUnit);
-    const lines = drawIsoLines(noiseData);
+		const intervals = linspace(lineFrequency, gridUnit);
+		const drawIsoLines = getIsoLineFn(intervals, gridUnit, [width, height]);
 
-    ctx.strokeStyle = lineColour;
-    ctx.lineWidth = lineWidth;
+		const noiseData = makeNoise(noise2D, gridUnit);
+		const lines = drawIsoLines(noiseData);
 
-    lines.forEach(([start, ...pts]: PolyLine) => {
-      ctx.beginPath();
-      ctx.moveTo(...start);
-      pts.forEach((pt) => ctx.lineTo(...pt));
-      ctx.stroke();
-    });
-  }
+		ctx.strokeStyle = lineColour;
+		ctx.lineWidth = lineWidth;
+
+		lines.forEach(([start, ...pts]: PolyLine) => {
+			ctx.beginPath();
+			ctx.moveTo(...start);
+			pts.forEach((pt) => ctx.lineTo(...pt));
+			ctx.stroke();
+		});
+	}
 }
--- src/paintlets/hexo/src/worklet.ts
+++ src/paintlets/hexo/src/worklet.ts
@@ -1,71 +1,73 @@
-import SimplexNoise from "simplex-noise";
+import type * as houdini from "../../../../typings/houdini";
 
-import * as houdini from "../../../../typings/houdini";
+import { createNoise2D } from "simplex-noise";
+
 import { normaliseInput } from "../../../_lib/utils";
 
 interface FillProps {
-  h: string;
-  s: string;
-  l: string;
-  a: string;
+	h: string;
+	s: string;
+	l: string;
+	a: string;
 }
 
 interface PaintletProps {
-  style: string;
-  radius: number;
-  gap: number;
-  fill: string;
-  strokeWidth: number;
-  strokeColour: string;
-  cubeTints: [string, string, string];
+	style: string;
+	radius: number;
+	gap: number;
+	fill: string;
+	strokeWidth: number;
+	strokeColour: string;
+	cubeTints: [string, string, string];
+  scale?: number;
 }
 
 interface HexProps {
-  x: number;
-  y: number;
-  rad: number;
+	x: number;
+	y: number;
+	rad: number;
 }
 
 export const defaultProps = {
-  "--style": {
-    key: "style",
-    value: "overlay",
-    parseAs: "string",
-  },
-  "--radius": {
-    key: "radius",
-    value: 16,
-    parseAs: "int",
-  },
-  "--gap": {
-    key: "gap",
-    value: 0,
-    parseAs: "float",
-  },
-  "--fill": {
-    key: "fill",
-    value: "",
-    parseAs: "string",
-  },
-  "--stroke-width": {
-    key: "strokeWidth",
-    value: 0,
-    parseAs: "float",
-  },
-  "--stroke-colour": {
-    key: "strokeColour",
-    value: "#fff",
-    parseAs: "string",
-  },
-  "--cube-tints": {
-    key: "cubeTints",
-    parseAs: "colours",
-  },
-  "--scale": {
-    key: "scale",
-    value: 0.001,
-    parseAs: "float",
-  },
+	"--style": {
+		key: "style",
+		value: "overlay",
+		parseAs: "string",
+	},
+	"--radius": {
+		key: "radius",
+		value: 16,
+		parseAs: "int",
+	},
+	"--gap": {
+		key: "gap",
+		value: 0,
+		parseAs: "float",
+	},
+	"--fill": {
+		key: "fill",
+		value: "",
+		parseAs: "string",
+	},
+	"--stroke-width": {
+		key: "strokeWidth",
+		value: 0,
+		parseAs: "float",
+	},
+	"--stroke-colour": {
+		key: "strokeColour",
+		value: "#fff",
+		parseAs: "string",
+	},
+	"--cube-tints": {
+		key: "cubeTints",
+		parseAs: "colours",
+	},
+	"--scale": {
+		key: "scale",
+		value: 0.001,
+		parseAs: "float",
+	},
 };
 
 const ANGLE = (2 * Math.PI) / 6;
@@ -73,182 +75,182 @@ const ANGLE = (2 * Math.PI) / 6;
 const hslRegex = /hsla?\((\d{1,3}),\s*(\d{1,3})%,\s*(\d{1,3})%,?\s?(\d*\.?\d+)?\)/g;
 
 function parseFill(fill: string): FillProps | undefined {
-  if (fill.length) {
-    const [h, s, l, a] = [...fill.matchAll(hslRegex)][0].slice(1);
-    return { h, s, l, a };
-  }
+	if (fill.length) {
+		const [h, s, l, a] = [...fill.matchAll(hslRegex)][0].slice(1);
+		return { h, s, l, a };
+	}
 }
 
 function getHexPath({ x, y, rad }: HexProps) {
-  const path = new Path2D();
-  for (let i = 0; i < 6; i++) {
-    path.lineTo(x + rad * Math.cos(ANGLE * i), y + rad * Math.sin(ANGLE * i));
-  }
-  path.closePath();
+	const path = new Path2D();
+	for (let i = 0; i < 6; i++) {
+		path.lineTo(x + rad * Math.cos(ANGLE * i), y + rad * Math.sin(ANGLE * i));
+	}
+	path.closePath();
 
-  return path;
+	return path;
 }
 
 function getCubeFaces({ x, y, rad }: HexProps): [Path2D, Path2D, Path2D] {
-  const leftFace = new Path2D();
-  leftFace.lineTo(x + rad * Math.cos(ANGLE * 3), y + rad * Math.sin(ANGLE * 3));
-  leftFace.lineTo(x + rad * Math.cos(ANGLE * 4), y + rad * Math.sin(ANGLE * 4));
-  leftFace.lineTo(x + rad * Math.cos(ANGLE * 5), y + rad * Math.sin(ANGLE * 5));
-  leftFace.lineTo(x, y);
-  leftFace.closePath();
+	const leftFace = new Path2D();
+	leftFace.lineTo(x + rad * Math.cos(ANGLE * 3), y + rad * Math.sin(ANGLE * 3));
+	leftFace.lineTo(x + rad * Math.cos(ANGLE * 4), y + rad * Math.sin(ANGLE * 4));
+	leftFace.lineTo(x + rad * Math.cos(ANGLE * 5), y + rad * Math.sin(ANGLE * 5));
+	leftFace.lineTo(x, y);
+	leftFace.closePath();
 
-  const topFace = new Path2D();
-  topFace.lineTo(x + rad * Math.cos(ANGLE * 1), y + rad * Math.sin(ANGLE * 1));
-  topFace.lineTo(x + rad * Math.cos(ANGLE * 2), y + rad * Math.sin(ANGLE * 2));
-  topFace.lineTo(x + rad * Math.cos(ANGLE * 3), y + rad * Math.sin(ANGLE * 3));
-  topFace.lineTo(x, y);
-  topFace.closePath();
+	const topFace = new Path2D();
+	topFace.lineTo(x + rad * Math.cos(ANGLE * 1), y + rad * Math.sin(ANGLE * 1));
+	topFace.lineTo(x + rad * Math.cos(ANGLE * 2), y + rad * Math.sin(ANGLE * 2));
+	topFace.lineTo(x + rad * Math.cos(ANGLE * 3), y + rad * Math.sin(ANGLE * 3));
+	topFace.lineTo(x, y);
+	topFace.closePath();
 
-  const rightFace = new Path2D();
-  rightFace.lineTo(x + rad * Math.cos(ANGLE * 5), y + rad * Math.sin(ANGLE * 5));
-  rightFace.lineTo(x + rad * Math.cos(ANGLE * 0), y + rad * Math.sin(ANGLE * 0));
-  rightFace.lineTo(x + rad * Math.cos(ANGLE * 1), y + rad * Math.sin(ANGLE * 1));
-  rightFace.lineTo(x, y);
-  rightFace.closePath();
+	const rightFace = new Path2D();
+	rightFace.lineTo(x + rad * Math.cos(ANGLE * 5), y + rad * Math.sin(ANGLE * 5));
+	rightFace.lineTo(x + rad * Math.cos(ANGLE * 0), y + rad * Math.sin(ANGLE * 0));
+	rightFace.lineTo(x + rad * Math.cos(ANGLE * 1), y + rad * Math.sin(ANGLE * 1));
+	rightFace.lineTo(x, y);
+	rightFace.closePath();
 
-  return [leftFace, topFace, rightFace];
+	return [leftFace, topFace, rightFace];
 }
 
 /**
  * Create a layer varying the hue
  */
 function rainbow(n: number) {
-  const h = Math.floor((300 * (n - 1)) / 2);
-  const hsl = `hsl(${h}, 50%, 70%)`;
-  return hsl;
+	const h = Math.floor((300 * (n - 1)) / 2);
+	const hsl = `hsl(${h}, 50%, 70%)`;
+	return hsl;
 }
 
 /**
  * Create a layer in the supplied colour (or black if not supplied), varying the lightness
  */
 function hue(n: number, fillProps?: FillProps) {
-  const { h, s, a } = { h: "0", s: "50", a: "1", ...fillProps };
-  const l = Math.floor(50 * n + 25) / 2;
-  const hsl = `hsl(${h}, ${s}%, ${l}%, ${a})`;
-  return hsl;
+	const { h, s, a } = { h: "0", s: "50", a: "1", ...fillProps };
+	const l = Math.floor(50 * n + 25) / 2;
+	const hsl = `hsl(${h}, ${s}%, ${l}%, ${a})`;
+	return hsl;
 }
 
 /**
  * Create a layer in the supplied colour (or black if not supplied), varying the transparency
  */
 function overlay(n: number, fillProps?: FillProps) {
-  const { h, s, l } = { h: "0", s: "0", l: "100", ...fillProps };
-  const a = (n - 0.75) / 2;
+	const { h, s, l } = { h: "0", s: "0", l: "100", ...fillProps };
+	const a = (n - 0.75) / 2;
 
-  const hsl = `hsla(${h}, ${s}%, ${l}%, ${a})`;
-  return hsl;
+	const hsl = `hsla(${h}, ${s}%, ${l}%, ${a})`;
+	return hsl;
 }
 
 function drawPathFn(
-  ctx: houdini.PaintRenderingContext2D,
-  { style, fill, strokeWidth }: PaintletProps
+	ctx: houdini.PaintRenderingContext2D,
+	{ style, fill, strokeWidth }: PaintletProps
 ) {
-  let fillProps: FillProps | undefined;
-  if (fill.length) fillProps = parseFill(fill);
+	let fillProps: FillProps | undefined;
+	if (fill.length) fillProps = parseFill(fill);
 
-  return function (path: Path2D, n: number) {
-    switch (style) {
-      case "rainbow":
-        ctx.fillStyle = rainbow(n);
-        ctx.fill(path);
-        break;
+	return function (path: Path2D, n: number) {
+		switch (style) {
+			case "rainbow":
+				ctx.fillStyle = rainbow(n);
+				ctx.fill(path);
+				break;
 
-      case "rainbow-stroke":
-        ctx.strokeStyle = rainbow(n);
-        ctx.stroke(path);
-        break;
+			case "rainbow-stroke":
+				ctx.strokeStyle = rainbow(n);
+				ctx.stroke(path);
+				break;
 
-      case "hue":
-        if (fillProps) {
-          ctx.fillStyle = hue(n, fillProps);
-          ctx.fill(path);
-        }
-        if (strokeWidth > 0) {
-          ctx.stroke(path);
-        }
-        break;
+			case "hue":
+				if (fillProps) {
+					ctx.fillStyle = hue(n, fillProps);
+					ctx.fill(path);
+				}
+				if (strokeWidth > 0) {
+					ctx.stroke(path);
+				}
+				break;
 
-      case "overlay":
-      default:
-        if (fillProps) {
-          ctx.fillStyle = overlay(n, fillProps);
-          ctx.fill(path);
-        }
-        if (strokeWidth > 0) {
-          ctx.stroke(path);
-        }
-        break;
-    }
-  };
+			case "overlay":
+			default:
+				if (fillProps) {
+					ctx.fillStyle = overlay(n, fillProps);
+					ctx.fill(path);
+				}
+				if (strokeWidth > 0) {
+					ctx.stroke(path);
+				}
+				break;
+		}
+	};
 }
 
 function drawCubeFaces(
-  [leftFace, rightFace, topFace]: [Path2D, Path2D, Path2D],
-  [leftFill, rightFill, topFill]: [string, string, string],
-  ctx: houdini.PaintRenderingContext2D
+	[leftFace, rightFace, topFace]: [Path2D, Path2D, Path2D],
+	[leftFill, rightFill, topFill]: [string, string, string],
+	ctx: houdini.PaintRenderingContext2D
 ) {
-  ctx.fillStyle = topFill;
-  ctx.fill(topFace);
+	ctx.fillStyle = topFill;
+	ctx.fill(topFace);
 
-  ctx.fillStyle = leftFill;
-  ctx.fill(leftFace);
+	ctx.fillStyle = leftFill;
+	ctx.fill(leftFace);
 
-  ctx.fillStyle = rightFill;
-  ctx.fill(rightFace);
+	ctx.fillStyle = rightFill;
+	ctx.fill(rightFace);
 }
 
 // TODO: allow "pointy" mode
 export class Hexo implements houdini.PaintCtor {
-  public static defaultProperties = defaultProps;
-  public static inputProperties = Object.keys(Hexo.defaultProperties);
+	public static defaultProperties = defaultProps;
+	public static inputProperties = Object.keys(Hexo.defaultProperties);
 
-  paint(
-    ctx: houdini.PaintRenderingContext2D,
-    { width, height }: houdini.PaintSize,
-    rawProps: houdini.StylePropertyMapReadOnly
-  ): void {
-    // TODO make normaliseInput generic
-    const props = normaliseInput(rawProps, Hexo) as PaintletProps;
-    const drawPath = drawPathFn(ctx, props);
+	paint(
+		ctx: houdini.PaintRenderingContext2D,
+		{ width, height }: houdini.PaintSize,
+		rawProps: houdini.StylePropertyMapReadOnly
+	): void {
+		// TODO make normaliseInput generic
+		const props = normaliseInput(rawProps, Hexo) as PaintletProps;
+		const drawPath = drawPathFn(ctx, props);
 
-    const { radius: r, gap, strokeWidth, strokeColour, cubeTints, scale } = props;
-    const noise = new SimplexNoise();
+		const { radius: r, gap, strokeWidth, strokeColour, cubeTints, scale } = props;
 
-    const rMin = r * Math.sin(ANGLE);
-    const rowH = rMin * 2;
-    const colW = r * 1.5;
-    const rowNum = 1 + Math.ceil(height / rowH);
-    const colNum = 1 + Math.ceil(width / colW);
+		const noise2D = createNoise2D();
+		const rMin = r * Math.sin(ANGLE);
+		const rowH = rMin * 2;
+		const colW = r * 1.5;
+		const rowNum = 1 + Math.ceil(height / rowH);
+		const colNum = 1 + Math.ceil(width / colW);
 
-    ctx.lineWidth = strokeWidth;
-    ctx.strokeStyle = strokeColour;
+		ctx.lineWidth = strokeWidth;
+		ctx.strokeStyle = strokeColour;
 
-    for (let i = 0; i < rowNum; i++) {
-      const y = i * rowH;
+		for (let i = 0; i < rowNum; i++) {
+			const y = i * rowH;
 
-      for (let j = 0; j < colNum; j++) {
-        const x = j * colW;
-        const yOff = j % 2 === 0 ? 0 : rMin;
-        const hexProps = {
-          x,
-          y: y + yOff,
-          rad: r - gap,
-        };
+			for (let j = 0; j < colNum; j++) {
+				const x = j * colW;
+				const yOff = j % 2 === 0 ? 0 : rMin;
+				const hexProps = {
+					x,
+					y: y + yOff,
+					rad: r - gap,
+				};
 
-        const hexPath = getHexPath(hexProps);
-        const n = noise.noise2D(scale * x, scale * y) + 1;
-        drawPath(hexPath, n);
+				const hexPath = getHexPath(hexProps);
+				const n = noise2D(scale * x, scale * y) + 1;
+				drawPath(hexPath, n);
 
-        if (cubeTints) {
-          const cubePaths = getCubeFaces(hexProps);
-          drawCubeFaces(cubePaths, cubeTints, ctx);
-        }
-      }
-    }
-  }
+				if (cubeTints) {
+					const cubePaths = getCubeFaces(hexProps);
+					drawCubeFaces(cubePaths, cubeTints, ctx);
+				}
+			}
+		}
+	}
 }
